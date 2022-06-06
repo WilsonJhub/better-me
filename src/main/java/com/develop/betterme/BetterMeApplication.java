10c439b8d192c1a2777e08c56a10861cbe3c2b81
wilsonjhub <jasonstimphil@gmail.com>
2022-06-06 17:55:36 -0400

    added starter code for app betterme

---
+++ src/main/java/com/develop/betterme/BetterMeApplication.java
@@ -0,0 +1,15 @@
+package com.develop.betterme;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// FRAMEWORK REQUIRED
+@SpringBootApplication
+public class BetterMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/betterme/BetterMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.betterme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
